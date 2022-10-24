2ed0e36d2ec6d8fb710673aba52d1df81585e4af
Nazmul Sujon <sujon686500@gmail.com>
2022-10-25 03:57:30 +0600

    create express js app & run it on server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use = cors();
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hero Coders API is running");
+});
+
+app.listen(port, () => {
+  console.log("server is running", port);
+});
